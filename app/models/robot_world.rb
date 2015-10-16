272453fea9dcb0311065ed2e25aa98b339d74f89
Matt Stjernholm <thew88@gmail.com>
2015-10-15 18:09:30 -0600

    fixed dashboard variables to work with new database

--- app/models/robot_world.rb
+++ app/models/robot_world.rb
@@ -18,12 +18,6 @@ class RobotWorld
     dataset.insert(robot)
   end
 
-  def self.raw_robots
-    database.transaction do
-      database['robots'] || []
-    end
-  end
-
   def self.all
     robots = dataset.to_a
     robots.map {|robot| Robot.new(robot)}
@@ -45,18 +39,19 @@ class RobotWorld
 
   def self.average_age
     current_year = Time.now.year
-    robots = raw_robots.map { |data| Robot.new(data) }
+    robots = RobotWorld.all
     array_of_birth_years = robots.map {|robot| robot.birthday.split("/").last.to_i}
     array_of_ages = array_of_birth_years.map! {|year| current_year - year}
     average = (array_of_ages.inject(0, :+))/(array_of_ages.size)
   end
 
   def self.hired_by_year
-    robots = raw_robots.map { |data| Robot.new(data) }
+    robots = RobotWorld.all
     hired_years = robots.map {|robot| robot.date_hired.split("/").last.to_i}
     keys = hired_years.uniq
     hash = {}
     keys.map {|key| hash[key] = hired_years.count(key)}
+    #sort hash by keys
     hash
   end
 
